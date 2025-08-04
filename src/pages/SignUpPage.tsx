b8050ed228dc194d0ce2ad14a57da53f988faa9b
Anthony Duong <khangbeo2012@gmail.com>
2025-08-04 03:02:46 -0700

    added bare bones signup page

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,4 +1,27 @@
+import { useAuth } from "../context/AuthContext";
+
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  const { signInWithGithub } = useAuth();
+  return (
+    <div>
+      <h2>Create Account</h2>
+      <div>
+        <button
+          onClick={signInWithGithub}
+          className="bg-blue-500 px-3 py-1 rounded cursor-pointer hover:bg-blue-600 hover:transition-colors duration-300"
+        >
+          Sign In With Github
+        </button>
+      </div>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Create password" />
+        <input type="text" placeholder="Confirm password" />
+        <button>Sign Up</button>
+        <p>Already have an account? Login</p>
+      </form>
+    </div>
+  );
 };
 export default SignUpPage;
